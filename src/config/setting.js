f9f24ad70b0e9dfaf1de4e51b0479caa8d1e8bfc
MinhNQA <nghiemminh1904@gmail.com>
2021-04-15 15:38:32 +0700

    new

--- src/config/setting.js
+++ src/config/setting.js
@@ -16,6 +16,7 @@ export const DataGetSubject = {
   idNam: 1,
 };
 
-// export const API_URL = 'http://192.168.1.101/api/ApiMobile';
-export const API_URL = 'http://172.17.96.1/api/ApiMobile';
+// export const API_URL = 'http://192.168.0.102/api/ApiMobile';
+// export const API_URL = 'http://172.17.96.1/api/ApiMobile';
+export const API_URL = 'http://10.13.129.5/api/ApiMobile';
 
